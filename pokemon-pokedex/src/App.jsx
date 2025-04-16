3fbd7750735dde5019f4bfacf0e2eda2c0d46c27
Ghiyath <ghiyathal101@gmail.com>
2025-04-16 13:06:44 +0200

    fix router

--- pokemon-pokedex/src/App.jsx
+++ pokemon-pokedex/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Pokedex from './pages/Pokedex';
 import PokemonInfo from './pages/PokemonInfo';
